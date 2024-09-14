7114dae179988dec8c75a90f4d0919ab58449ca9
Mrutunjay <inmadar.luu0c@passinbox.com>
2024-09-14 12:06:07 +0530

    Added build level config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   // server:}{}
-})
+  build: {
+    rollupOptions: {
+      external: ["firebase/app"] // Add any other external dependencies here
+    }
+  }
+});
